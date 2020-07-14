fcb249feb656fb327fccad637a61f89afb942dd2
zetaemmesoft <zetaemmesoft@gmail.com>
2020-07-14 16:49:39 +0200

    update init db

--- zetaemmesoft-monitor/src/main/resources/db-schema.sql
+++ zetaemmesoft-monitor/src/main/resources/db-schema.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS ITEM_TYPE (ID INT NOT NULL PRIMARY KEY,
                                       NAME VARCHAR(50) NOT NULL UNIQUE);
 
 CREATE TABLE IF NOT EXISTS SENSOR (ID INT NOT NULL PRIMARY KEY, 
+                                   NODE VARCHAR(15) NOT NULL, 
                                    NAME VARCHAR(50) NOT NULL, 
                                    ITEM_TYPE INT NOT NULL,                                    
-                                   NODE VARCHAR(15) NOT NULL, 
                                    VALUE double, 
                                    UNIT VARCHAR(5), 
                                    TIME TIMESTAMP, 
@@ -17,9 +17,9 @@ CREATE TABLE IF NOT EXISTS SENSOR (ID INT NOT NULL PRIMARY KEY,
 ALTER TABLE SENSOR ADD FOREIGN KEY (ITEM_TYPE) REFERENCES ITEM_TYPE (ID);                               
 
 CREATE TABLE IF NOT EXISTS SWITCH (ID INT NOT NULL PRIMARY KEY, 
+                                   NODE VARCHAR(15) NOT NULL, 
                                    NAME VARCHAR(50) NOT NULL, 
                                    ITEM_TYPE INT NOT NULL,                                    
-                                   NODE VARCHAR(15) NOT NULL, 
                                    VALUE INT, 
                                    UNIT VARCHAR(5), 
                                    TIME TIMESTAMP, 
